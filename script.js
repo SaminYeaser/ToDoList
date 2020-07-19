e14097bc85d543e3cc59aa8a5f353c91c1a33e59
Samin <saminyeaser1@gmail.com>
2020-07-19 19:34:23 +0600

    listings added

---
+++ script.js
@@ -0,0 +1,9 @@
+//selector
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event listener
+
+
+//functions
