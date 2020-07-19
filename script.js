43b5b97e3341b42b815e798e6f80be7d2148164a
Samin <saminyeaser1@gmail.com>
2020-07-20 00:37:03 +0600

    creating buttons

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ todoButton.addEventListener('click',addTodo)
 function addTodo() {
     event.preventDefault();
     const toDiv = document.createElement('div')
-    toDiv.classList.add('todo')
+    toDiv.classList.add('todo');
+    const newTodo = document.createElement('li');
+    newTodo.innerText ='hey';
+    newTodo.classList.add('todo-item');
+    toDiv.append(newTodo);
+
+    //completed button
+
+    const completdButton = document.createElement('button');
+    completdButton.innerHTML = '<i class="fa fa-plus"></i>'
+    completdButton.classList('complete-btn');
+    toDiv.append(completdButton);
+
+    //delete button
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa fa-minus"></i>'
+    deleteButton.classList('complete-btn');
+    toDiv.append(completdButton);
 }
