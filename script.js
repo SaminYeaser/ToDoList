1f6235970da3f4f89d1bc57f4cdf12de3e605ca1
Samin <saminyeaser1@gmail.com>
2020-07-22 02:56:24 +0600

    working little bit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 //Event listener
-
+document.addEventListener('DOMContentLoaded', getTodos)
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click', deleteTodo);
 // filterOption.addEventListener('click',filterTodo)
